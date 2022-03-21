cb6445d6e0f499cb8b4d5332e36160e41f845701
Oguzhan Dogan <oguzhaneee@gmail.com>
2022-03-21 22:15:48 +0300

    Update ResumeNew.js

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row,Col } from "react-bootstrap";
+import { Container} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 
 import pdf from "../../Assets/../Assets/Oguzhan_Dogan_CV.pdf";
